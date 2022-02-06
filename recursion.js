e8ff505c75d8ee6c12c490c6c49a863a4c7983ae
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-02-06 07:08:30 +0600

    Update recursion

--- recursion.js
+++ recursion.js
@@ -27,6 +27,14 @@ function sumReverse(getEndingNumber, getStartingNumber){
     return getEndingNumber + sumReverse(--getEndingNumber, getStartingNumber);
 }
 
+// --Simple sum function program using recursion--
+function factorial(getNumber){
+    if(getNumber == 1){
+        return 1;
+    }
+    return getNumber * factorial(--getNumber);
+}
+
 // -----All Functions Are End Here-----
 
 const startingNumber = 1;
@@ -37,11 +45,21 @@ console.log("-----Sum Program (Forword)-----");
 console.log(sumForword(startingNumber, endingNumber));
 console.log("-----Sum Program (Reverse)-----");
 console.log(sumReverse(endingNumber, startingNumber));
+console.log("-----Find Factorial-----");
+const number = 5;
+console.log(factorial(number));
 
-// -----Details overview of recursion-----
+// -----Details overview of Sum process using recursion-----
 // 5 + sumReverse(4, 1)
 // 5 + 4 + sumReverse(3, 1)
 // 5 + 4 + 3 + sumReverse(2, 1)
 // 5 + 4 + 3 + 2 + sumReverse(1, 1)
 // 5 + 4 + 3 + 2 + 1 + sumReverse(0, 1)
 // 5 + 4 + 3 + 2 + 1 + 0 = 15
+
+// -----Details overview of find factorial using recursion-----
+// 5 * factorial(4, 1)
+// 5 * 4 * factorial(3, 1)
+// 5 * 4 * 3 * factorial(2, 1)
+// 5 * 4 * 3 * 2 * factorial(1, 1)
+// 5 * 4 * 3 * 2 * 1 = 120
